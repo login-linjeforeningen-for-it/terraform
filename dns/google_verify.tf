b2371a3bce1eeeae7fb8ee82a67b061c3d8bf9a1
0herm <0herm@protonmail.com>
2026-09-03 17:07:26 +0200

    feat: update TTL settings across DNS records and remove unused variables

--- dns/google_verify.tf
+++ dns/google_verify.tf
@@ -2,7 +2,7 @@ resource "ovh_domain_zone_record" "google_verify_root" {
   zone      = var.login
   subdomain = ""
   fieldtype = "TXT"
-  ttl       = local.ttl_low
+  ttl       = var.ttl
   target    = join(" ", formatlist("%q", regexall(".{1,255}", "google-site-verification=WDHZtpjiTEsnySmCP-lQUQibvV7pFbHcIC4cG_gWUxU")))
 }
 
@@ -10,6 +10,6 @@ resource "ovh_domain_zone_record" "google_verify_forms" {
   zone      = var.login
   subdomain = "forms"
   fieldtype = "TXT"
-  ttl       = local.ttl_low
+  ttl       = var.ttl
   target    = join(" ", formatlist("%q", regexall(".{1,255}", "google-site-verification=ryrYCeqvEF5EDnpjioRq1DIyY6PByK-LbtkFwcI7m-c")))
 }
--- dns/logntnu.tf
+++ dns/logntnu.tf
@@ -2,7 +2,7 @@ resource "ovh_domain_zone_record" "logntnu_discord" {
   zone      = "logntnu.no"
   subdomain = "discord"
   fieldtype = "CNAME"
-  ttl       = local.ttl_low
+  ttl       = var.ttl
   target    = "discord.login.no."
 }
 
@@ -10,7 +10,7 @@ resource "ovh_domain_zone_record" "logntnu_autodiscover" {
   zone      = "logntnu.no"
   subdomain = "autodiscover"
   fieldtype = "CNAME"
-  ttl       = local.ttl_low
+  ttl       = var.ttl
   target    = "autodiscover.emailsrvr.com."
 }
 
@@ -18,7 +18,7 @@ resource "ovh_domain_zone_record" "logntnu_git_spf" {
   zone      = "logntnu.no"
   subdomain = "git"
   fieldtype = "TXT"
-  ttl       = local.ttl_low
+  ttl       = var.ttl
   target    = join(" ", formatlist("%q", regexall(".{1,255}", "v=spf1 include:emailsrvr.com ~all")))
 }
 
@@ -26,7 +26,7 @@ resource "ovh_domain_zone_record" "logntnu_git_mx1" {
   zone      = "logntnu.no"
   subdomain = "git"
   fieldtype = "MX"
-  ttl       = local.ttl_low
+  ttl       = var.ttl
   target    = "10 mx1.emailsrvr.com."
 }
 
@@ -34,7 +34,7 @@ resource "ovh_domain_zone_record" "logntnu_git_mx2" {
   zone      = "logntnu.no"
   subdomain = "git"
   fieldtype = "MX"
-  ttl       = local.ttl_low
+  ttl       = var.ttl
   target    = "20 mx2.emailsrvr.com."
 }
 
@@ -42,6 +42,6 @@ resource "ovh_domain_zone_record" "logntnu_git_dmarc" {
   zone      = "logntnu.no"
   subdomain = "_dmarc.git"
   fieldtype = "TXT"
-  ttl       = local.ttl_low
+  ttl       = var.ttl
   target    = join(" ", formatlist("%q", regexall(".{1,255}", "v=DMARC1; p=quarantine; rua=mailto:tekkom@logntnu.no")))
 }
